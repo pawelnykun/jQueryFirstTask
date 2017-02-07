3a5e7914d7d6cdc4bad39235cf194360b042677b
pawelnykun <pawel.nykun@gmail.com>
2017-02-07 09:57:33 +0100

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,8 @@ span.each(function(index, element) {
 
 var paragraphs = $('p');
 paragraphs.each(function(index, element) {
-	var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
-	$(element).append(button)
+	var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
+	$(element).append(button);
 });
 
 $("button").click(function(){
